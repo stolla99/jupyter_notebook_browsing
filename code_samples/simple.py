5c0875b662caa130d6f97b3bb147eedd807ea0c1
Arne <arne.stoll.1@outlook.de>
2021-12-28 15:04:18 +0100

    Simple graph visualization of AST

---
+++ code_samples/simple.py
@@ -0,0 +1,3 @@
+num = 8
+h = 50
+erg = num + h
---
+++ main/main.py
@@ -0,0 +1,87 @@
+import ast
+import os
+import networkx as nx
+import matplotlib.pyplot as plt
+from colorama import Fore, Back, Style
+from networkx.drawing.nx_agraph import graphviz_layout
+
+
+def node_str_generator(node):
+    print("dd")
+    return type(node).__name__ + str(node)
+
+
+# Traverse over tree and add every new node to the list and extend edge list with every
+# traversal in child nodes
+def node_traversal(nd):
+    node_str = node_str_generator(nd)
+    if not (node_str in node_list):
+        node_list.append(node_str)
+    if isinstance(nd, ast.AST):
+        lst = ast.iter_fields(nd)
+        for field, value in lst:
+            if isinstance(value, list):
+                for child in value:
+                    if isinstance(child, ast.AST):
+                        edge_list.append((node_str_generator(nd), node_str_generator(child)))
+                        node_traversal(child)
+            elif isinstance(value, ast.AST):
+                edge_list.append((node_str_generator(nd), node_str_generator(value)))
+                node_traversal(value)
+
+
+# Lists of node in AST and edges
+node_list = []
+edge_list = []
+# Define root directory and get list of files in that specific directory
+root = '../code_samples/'
+files = os.listdir(path=root)
+
+# Print filenames with byte-size in console
+print(Back.LIGHTBLACK_EX + Fore.RED + 'Select file index to use:' + Style.RESET_ALL)
+for index, file in enumerate(files):
+    # Index, Name, Bytes
+    print(index, file, os.path.getsize(root + file))
+
+# TODO: file index user input
+file_in_use = files[0]
+# File which is later read
+file = os.open(root + file_in_use, os.O_RDONLY)
+num_bytes = os.path.getsize(root + file_in_use)
+
+# File as byte string
+byte_str = os.read(file, num_bytes).decode("UTF-8")
+print("\n" + Back.LIGHTBLACK_EX + Fore.RED + "Program code currently in use for AST:" + Style.RESET_ALL)
+print(byte_str)
+
+# Parse AST and traverse the tree by going over every node from source/root
+ast_tree = ast.parse(source=byte_str)
+node_traversal(ast_tree)
+
+# Create graph from edge list and nodelist using the dot-layout
+# TODO: Style tree such it can be read properly
+plt.title('AST, file: ' + file_in_use)
+
+
+graph = nx.DiGraph()
+graph.add_edges_from(edge_list)
+pos = graphviz_layout(graph, prog='dot')
+labels = dict()
+for node in node_list:
+    labels[node] = node.split('<')[0]
+nx.draw(graph, pos,
+        with_labels=True,
+        labels=labels,
+        edgecolors='none',
+        node_color='lightgray',
+        font_size=10,
+        # so^>v<dph8
+        node_shape='o',
+        node_size=400)
+
+# Save plot in file and show in IDE
+plt.savefig('ast_' + file_in_use + '.png')
+plt.show()
+
+# Close file reader
+os.close(file)
